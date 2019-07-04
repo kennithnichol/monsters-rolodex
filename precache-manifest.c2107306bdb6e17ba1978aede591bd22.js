c99e4ebb786e08791a39f20b59654ca2a0710263
Ken Nichol <ken.nichol@live.ca>
2019-07-04 10:42:32 -0700

    Updates

---
+++ precache-manifest.c2107306bdb6e17ba1978aede591bd22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e20b49bd807fcffb3d6cc0f6eb69dcf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "acf37dcf29b156d2e558",
+    "url": "/monsters-rolodex/static/css/main.feee2f6e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "acf37dcf29b156d2e558",
+    "url": "/monsters-rolodex/static/js/main.3d75ae04.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
